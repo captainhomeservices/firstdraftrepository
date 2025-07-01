bdb2fcd81164e8c8fad8ddc3aa3c08dac182ea46
captainhomeservices <captainhomeservices@gmail.com>
2025-06-30 22:14:28 -0600

    Updated Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import { Menu, X, Wheel } from 'lucide-react';
+import { Menu, X, ShipWheel as Wheel } from 'lucide-react';
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
